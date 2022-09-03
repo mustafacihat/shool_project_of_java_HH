7be448395f4bc46f8f9c5a6dda7d5e986e86e61c
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-09-03 15:20:42 +0200

    added new instructions to Implementation_Guide.txt and updated School.java

--- src/main/java/com/cydeo/entity/School.java
+++ src/main/java/com/cydeo/entity/School.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 /**
  * I could not see any progress on the service methods of School entity Esmira.
+ * I could not see any progress on the service methods of School entity Alena.
+ * Come together your pairs and try to write your code.
+ * You can check your codes inside main method which created inside SchoolMainRunner
  */
 
 @Getter
