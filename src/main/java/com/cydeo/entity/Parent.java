08a4dd3fe94beaadd8b148b452df86d9836ed8b3
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-09-03 15:29:41 +0200

    added new instructions to Implementation_Guide.txt and updated Parent.java

--- src/main/java/com/cydeo/entity/Parent.java
+++ src/main/java/com/cydeo/entity/Parent.java
@@ -2,6 +2,13 @@ package com.cydeo.entity;
 
 import lombok.*;
 
+/**
+ * I could not see any progress on the service methods of Parent entity Marika.
+ * I could not see any progress on the service methods of Parent entity Sergiy.
+ * Come together your pairs and try to write your code.
+ * You can check your codes inside main method which created inside SchoolMainRunner
+ */
+
 @Getter
 @Setter
 @NoArgsConstructor
