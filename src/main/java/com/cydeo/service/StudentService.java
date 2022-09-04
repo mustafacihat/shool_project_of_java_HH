f12b146f0e2239a8fca2f208fa5747af5771870d
mizgin <mizgincvk@gmail.com>
2022-09-04 18:11:26 -0400

    StudentService.java created and updated

--- src/main/java/com/cydeo/service/StudentService.java
+++ src/main/java/com/cydeo/service/StudentService.java
@@ -3,11 +3,8 @@ package com.cydeo.service;
 import com.cydeo.database.Database;
 import com.cydeo.entity.Student;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 public class StudentService implements CRUDService<Student>{
     @Override
@@ -39,6 +36,8 @@ public class StudentService implements CRUDService<Student>{
     public void deleteById(Long id) {
         Database.studentList.removeIf(student -> student.getId()==id);
         System.out.println(Database.studentList);
+
+
 //        Database.studentList.stream()
 //                .filter(student -> student.getId() == id)
 //                .findFirst()
