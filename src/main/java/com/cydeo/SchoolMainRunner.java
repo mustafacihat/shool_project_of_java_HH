d4dde5d11dcf3b38c53ec9b6c1b65da6df4c407a
Sabina Omarova <sabomar170@gmail.com>
2022-09-08 10:52:20 -0400

    SchoolMainRunner courseService test

--- src/main/java/com/cydeo/SchoolMainRunner.java
+++ src/main/java/com/cydeo/SchoolMainRunner.java
@@ -1,20 +1,46 @@
 package com.cydeo;
 
+import com.cydeo.database.Database;
+import com.cydeo.entity.Course;
+import com.cydeo.enums.WeekDays;
+import com.cydeo.service.CRUDService;
+import com.cydeo.service.CourseService;
+
+import java.util.List;
+
 public class SchoolMainRunner {
 
 //    public static CRUDService<School> schoolService;
-//    public static CRUDService<Course> courseService;
+   public static CRUDService<Course> courseService;
 //    public static CRUDService<Parent> parentService;
 //    public static CRUDService<Student> studentService;
 //
-//    static {
-//        SchoolMainRunner.schoolService = new SchoolService();
-//        SchoolMainRunner.courseService = new CourseService();
+    static {
+ //       SchoolMainRunner.schoolService = new SchoolService();
+        SchoolMainRunner.courseService = new CourseService();
 //        SchoolMainRunner.parentService = new ParentService();
 //        SchoolMainRunner.studentService = new StudentService();
-//    }
+   }
 
     public static void main(String[] args) {
+
+        System.out.println("---------findById--------");
+        System.out.println(courseService.findById(1));
+
+        System.out.println("----------findAll--------");
+        System.out.println(courseService.findAll());
+
+        System.out.println("-----------save-----------");
+        courseService.save(new Course(4,"French", 40, List.of(WeekDays.MONDAY, WeekDays.THURSDAY)));
+        System.out.println(Database.courseList);
+
+        System.out.println("----------update---------");
+        courseService.update(new Course(1,"Spanish", 40, List.of(WeekDays.MONDAY, WeekDays.THURSDAY)));
+        System.out.println(Database.courseList);
+
+        System.out.println("-----------deleteById---------");
+        courseService.deleteById(2L);
+        System.out.println(Database.courseList);
 
 
     }
